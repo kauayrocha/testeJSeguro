19fecd219daad0b4ad79f13bbf4ccb1786e395a7
Alan kauay <alan.oliveira@inflor.com.br>
2021-09-25 22:29:26 -0300

    apontando para o repositorio correto no ecr e liberando porta 8080 no inbound do .tf retirando o 443 pois ta sem certificado ssl.

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -54,8 +54,8 @@ resource "aws_security_group" "ecs_sg" {
     }
 
     ingress {
-        from_port       = 443
-        to_port         = 443
+        from_port       = 8080
+        to_port         = 8080
         protocol        = "tcp"
         cidr_blocks     = ["0.0.0.0/0"]
     }
