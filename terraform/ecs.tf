392d3d7f86c386a81b70624f90e70d381d5168a0
Alan kauay <alan.oliveira@inflor.com.br>
2021-09-26 13:41:37 -0300

    versionando task-definition-ecs-jseguro-node.json e adicionando recurso da task no terraform.

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -7,3 +7,16 @@ resource "aws_ecs_cluster" "ecs-juntoseguros" {
     
   }
 }
+
+resource "aws_ecs_task_definition" "task_definition" {
+  container_definitions    = data.template_file.task_definition_json.rendered                                                                                                          
+  family                   = "junto-task-defination"
+  network_mode             = "bridge"
+  memory                   = "2048"
+  cpu                      = "1024"
+  requires_compatibilities = ["EC2"]                                                                                                                                                                                                                                                                                                
+} 
+
+data "template_file" "task_definition_json" {
+  template = "${file("${path.module}/ecs/task-definition-ecs-jseguro-node.json")}"
+}
