fe85efc2e93cefb32a7017a6f07c56a0f8698d70
Alan kauay <alan.oliveira@inflor.com.br>
2021-09-26 14:19:19 -0300

    alterando circle ci para testar o update task definition pelo pipeline.

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_cluster" "ecs-juntoseguros" {
   }
 }
 
-resource "aws_ecs_task_definition" "task_definition" {
+resource "aws_ecs_task_definition" "tsk-node-juntoseguros" {
   container_definitions    = data.template_file.task_definition_json.rendered                                                                                                          
   family                   = "junto-task-defination"
   network_mode             = "bridge"
@@ -19,4 +19,16 @@ resource "aws_ecs_task_definition" "task_definition" {
 
 data "template_file" "task_definition_json" {
   template = "${file("${path.module}/ecs/task-definition-ecs-jseguro-node.json")}"
+}
+
+resource "aws_ecs_service" "node-juntoseguro" {
+  cluster                 = aws_ecs_cluster.ecs-juntoseguros.id
+  desired_count           = 1     
+  launch_type             = "EC2"
+  name                    = "service-juntoseguros"
+  task_definition         = aws_ecs_task_definition.tsk-node-juntoseguros.arn
+  force_new_deployment    = true
+  depends_on = [
+    aws_ecs_task_definition.tsk-node-juntoseguros
+  ]
 }
